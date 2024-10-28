202e8cea5e2b64a7de0dc50497d0815aa99dbeb3
Cmdr-Raygun <renttibwerdna@gmail.com>
2024-10-27 23:13:58 -0500

    W8 Files
    Sorry it's so late again!

--- program_1.py
+++ program_1.py
@@ -1,3 +1,6 @@
+# Author: Andrew Bittner
+# Date: 10/25/2024
+
 # Program #1: Initials
 # Write a program that gets a string containing a person's first, middle, and last names, 
 # and displays their first, middle, and last initials.  
@@ -5,15 +8,74 @@
 
 # Add your logic starting on line 11
 
-def initials_generator(personsName):
+def initials_generator(name_full):
+    name_full = validate_inp(name_full, str, None, ' ')
+    persons_initials = []
+    for name in name_full:
+        if not name.isalnum():
+            continue
+        ch = name[0] + '.'
+        persons_initials.append(ch)
+    persons_initials = ' '.join(persons_initials)
+    return persons_initials
 
-    personsInitials = ""
-    #    Add your logic here
 
-    return personsInitials.strip()
+def exit_sequence():
+    # Function to keep console/window open until user ends program.
+    input('\n\nPress [enter] to exit... ')
 
-personsName = input('Enter the users first, middle, and last name')
 
-initials = initials_generator(personsName)
+def validate_inp(inp, out_type, elem_ct=None, spl_ch=None, err_msg='Invalid input (was it formatted correctly?). ',
+                 inp_msg='Please re-enter your answer: '):
+    # Set the loop control variable.
+    repeat_cnv = True
+    while repeat_cnv:
+        #  Set the loop control variable to False so that loop does not run again on default.
+        repeat_cnv = False
+        try:
+            # Split the input into a list, if possible and desired.
+            if spl_ch is not None and type(inp) == str:
+                inp = inp.split(spl_ch)
+            # If the input type is not already list or tuple, convert variable to list for processing.
+            elif type(inp) != list and type(inp) != tuple:
+                inp = [inp]
+            # If elements in the input list do not meet desired element count, raise an error to execute re-input code.
+            if elem_ct is not None and elem_ct != len(inp):
+                raise ValueError
+            # Select an individual element from the input to process.
+            for elem in inp:
+                # If the input element's type is not already the desired output type, execute the conversion code.
+                while type(elem) != out_type:
+                    if out_type == int or float:
+                        # Select an individual character from the element to process.
+                        for ch in elem:
+                            # If the character is not a digit, remove it.
+                            if not ch.isdigit():
+                                elem = elem.replace(ch, '')
+                    # Convert the element to desired type and append it to the input list.
+                    elem = out_type(elem)
+                    inp.append(elem)
+            # Remove original, unconverted data left in input list.
+            for elem in inp.copy():
+                if type(elem) != out_type:
+                    inp.remove(elem)
+            # If input list contains only one item, break it out of the list.
+            if len(inp) > 0 and not len(inp) > 1:
+                inp = inp[0]
+        # If bad data results in a failing operation, run error handler.
+        except ValueError:
+            # Ask the user to input new data.
+            print(err_msg, end='')
+            inp = input(inp_msg)
+            repeat_cnv = True
+        return inp
 
-print(initials)
+
+def main():
+    initials = initials_generator(input('Enter the user\'s first, middle, and last name (separated by spaces): '))
+    print(initials)
+    exit_sequence()
+
+
+if __name__ == '__main__':
+    main()
--- program_3.py
+++ program_3.py
@@ -1,3 +1,6 @@
+# Author: Andrew Bittner
+# Date: 10/25/2024
+
 # Program #3: Capital Quiz
 # Write a program that creates a dictionary containing the U.S. states as keys, 
 # and their capitals as values.  
@@ -6,3 +9,57 @@
 # The program should count of the number of correct and incorrect responses.  
 # (You could alternatively use another country and provinces, 
 # or countries of the world and capitals).
+
+import random
+
+def exit_sequence():
+    # Function to keep console/window open until user ends program.
+    input('\n\nPress [enter] to exit... ')
+
+def quiz_user(answers, prompt, q_ct=5):
+    score = 0
+    for num in range(q_ct):
+        terms = random.sample(list(answers), k = q_ct)
+        key = terms[num]
+        value = answers[key]
+        prompt_new = prompt.replace('_TERM_', key)
+        response = input(prompt_new)
+        for ind in range(len(value)):
+            correct = False
+            if response.lower().strip() == value[ind].lower():
+                correct = True
+            if correct:
+                break
+        if correct:
+            print('That\'s correct!')
+            score += 1
+        else:
+            print('Sorry, that\'s incorrect.')
+        correct = False
+    print(f'You scored {score} out of {q_ct} points.')
+
+
+def main():
+    state_capitals = {'Alabama': ['Montgomery'], 'Alaska': ['Juneau'], 'Arizona': ['Phoenix'],
+                      'Arkansas': ['Little Rock'], 'California': ['Sacramento'], 'Colorado': ['Denver'],
+                      'Connecticut': ['Hartford'], 'Delaware': ['Dover'], 'Florida': ['Tallahassee'],
+                      'Georgia': ['Atlanta'], 'Hawaii': ['Honolulu'], 'Idaho': ['Boise'], 'Illinois': ['Springfield'],
+                      'Indiana': ['Indianapolis'], 'Iowa': ['Des Moines'], 'Kansas': ['Topeka'],
+                      'Kentucky': ['Frankfort'], 'Louisiana': ['Baton Rouge'], 'Maine': ['Augusta'],
+                      'Maryland': ['Annapolis'], 'Massachusetts': ['Boston'], 'Michigan': ['Lansing'],
+                      'Minnesota': ['St. Paul', 'Saint Paul'], 'Mississippi': ['Jackson'],
+                      'Missouri': ['Jefferson City'], 'Montana': ['Helena'], 'Nebraska': ['Lincoln'],
+                      'Nevada': ['Carson City'], 'New Hampshire': ['Concord'], 'New Jersey': ['Trenton'],
+                      'New Mexico': ['Santa Fe'], 'New York': ['Albany'], 'North Carolina': ['Raleigh'],
+                      'North Dakota': ['Bismarck'], 'Ohio': ['Columbus'], 'Oklahoma': ['Oklahoma City'],
+                      'Oregon': ['Salem'], 'Pennsylvania': ['Harrisburg'], 'Rhode Island': ['Providence'],
+                      'South Carolina': ['Columbia'], 'South Dakota': ['Pierre'], 'Tennessee': ['Nashville'],
+                      'Utah': ['Salt Lake City, Salt Lake'], 'Vermont': ['Montpelier'], 'Virginia': ['Richmond'],
+                      'Washington': ['Olympia'], 'West Virginia': ['Charleston'], 'Wisconsin': ['Madison'],
+                      'Wyoming': ['Cheyenne']}
+    quiz_user(state_capitals, 'What is the capital of _TERM_? ')
+    exit_sequence()
+
+
+if __name__ == '__main__':
+    main()
